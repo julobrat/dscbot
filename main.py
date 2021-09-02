9ebae1fe25fb30139613ebea0c83d0866294c4ee
BuildTools <unconfigured@null.spigotmc.org>
2021-09-02 10:30:05 +0200

    Minor visual changes i.e. function name change and imports grouping

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
+# only to work with replit's secrets and db
 import replit
-import os
+from replit import db
+# core functionalities
 import discord
 import googleapiclient.discovery
-from typing import List
 from keep_alive import keep_alive
-from datetime import datetime
-import pytz
-from replit import db
+# things that help in development
+import os
+from typing import List
 import help
 
-#TODO: zrób funkcję do wyświetlanie komentarzy z danego przedziału, w tym szczególny przypadek dla 1 komentarza np. 5. od końca
 
-class RequestParameters:
-    def __init__(self, videoId: str = None, part: str = None, textFormat: str = None, pageToken: str = None,
-                 maxResults: int = 20):
-        self.videoId = videoId
-        self.part = part
-        self.textFormat = textFormat
-        self.pageToken = pageToken
-        self.maxResults = maxResults
+# TODO: zrób funkcję do wyświetlanie komentarzy z danego przedziału, w tym szczególny przypadek dla 1 komentarza np. 5. od końca
 
 
 def getHelp() -> str:
@@ -41,6 +34,16 @@ def makeStrFromComments(comments: List) -> str:
     return msg
 
 
+class RequestParameters:
+    def __init__(self, videoId: str = None, part: str = None, textFormat: str = None, pageToken: str = None,
+                 maxResults: int = 20):
+        self.videoId = videoId
+        self.part = part
+        self.textFormat = textFormat
+        self.pageToken = pageToken
+        self.maxResults = maxResults
+
+
 class MyClient(discord.Client):
     def __init__(self, serviceName: str, version: str, developerKey: str):
         super().__init__()
@@ -55,22 +58,22 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print("Logged in as", self.user.name, self.user.id)
 
-    async def startWorking(self):
+    async def on_message(self, message):
+        if message.content.startswith('!'):
+            await self.resolveCommand(message)
+        if self.turnedOn:
+            await self.tryToAutomaticalyGetLatestCommentWhenItChanges()
+
+    async def tryToAutomaticalyGetLatestCommentWhenItChanges(self):
         self.requestParameters.pageToken = None
         comment = self.getLatestComments(1)
         if comment != self.latestComment:
             self.latestComment = comment
             await self.channelToShowLatestComment.send(makeStrFromComments(comment))
 
-    async def on_message(self, message):
-        if message.content.startswith('!'):
-            await self.resolveCommand(message)
-        if self.turnedOn:
-            await self.startWorking()
-
     async def resolveCommand(self, message):
         commandWords: List[str] = message.content[1:].split(sep=' ')
-        msg = "Unknown command"
+        msg = "It just doesn't work"
         if message.content.startswith('!greet'):
             msg = 'I greet you'
         if message.content.startswith('!comment'):
