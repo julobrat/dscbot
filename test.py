48e25c75d6341a50e1c211895006f896d097513f
Julian <72696235+julobrat@users.noreply.github.com>
2021-09-02 17:58:31 +0000

    Updated .gitignore and deleted useless test.py file

--- test.py
+++
@@ -1,9 +0,0 @@
-# from datetime import datetime
-# import pytz
-#
-# tz_Warsaw = pytz.timezone('Europe/Warsaw')
-# datetime_Warsaw = datetime.now(tz_Warsaw)
-# print(datetime_Warsaw.strftime("%H:%M:%S"))
-
-x, y= [1, 2, 3]
-print(x, y, z)
