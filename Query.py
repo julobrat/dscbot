31be6594da6d68337115cf13f48814cef2a3d88b
BuildTools <unconfigured@null.spigotmc.org>
2021-09-03 14:53:47 +0200

    Older comments are now at the beginning of the db, show has more options and !download is now !update that does the same job and also updates new comments.

--- Query.py
+++ Query.py
@@ -24,18 +24,40 @@ def convertCommentTextToStr(comment) -> str:
     return f"```{comment['snippet']['textDisplay']}```"
 
 
-def getCommandParameters(message: str) -> List[str]:
-    words = message.split(' ')
+def getCommandParametersWithSeparator(message: str, sep: str = ' ') -> List[str]:
+    words = message.split(sep)
     if len(words) > 1:
         return words[1:]
-    return ['0']
+    return []
+
+
+def dbCountAsString() -> str:
+    return str(db['count'])
+
+
+def isCommentFromDeletedChannel(comment) -> bool:
+    if comment['snippet']['authorChannelUrl'] == '':
+        return True
+    return False
+
+
+def isNumberOfParametersBetween(parameters: List, minimal: int = 0, maximal: int = 0) -> bool:
+    if minimal < 0 or maximal < 0:
+        raise ValueError(f'minimal {minimal} or maximal {maximal} are negative')
+    if minimal > maximal:
+        raise ValueError(f'Minimal {minimal} is greater than maximal {maximal}')
+    numberOfParameters = len(parameters)
+    if minimal <= numberOfParameters <= maximal:
+        return True
+    return False
 
 
 class PerformedQuery:
     class Target:
-        def __init__(self, videoId: str = None, channelId: str = None):
+        def __init__(self, videoId: str = None, channelId: str = None, idOfCommentToStopOn: str = None):
             self.videoId = videoId
             self.channelId = channelId
+            self.commentToStopOnId = idOfCommentToStopOn
 
     class RequestParameters:
         def __init__(self, part: str = None, textFormat: str = None, maxResults: int = 20, pageToken: str = None):
@@ -53,14 +75,10 @@ class PerformedQuery:
             except KeyError:
                 self.resetPageToken()
 
-
     def __init__(self, message: discord.Message):
         self.discordMessage: discord.Message = message
         self.target = None
         self.requestParameters = None
-
-    def setTarget(self, videoId: str, channelId: str):
-        self.target = self.Target(videoId, channelId)
 
     def setRequestParameters(self, part: str = None, textFormat: str = None, maxResults=20, pageToken: str = None):
         self.requestParameters = self.RequestParameters(part, textFormat, maxResults, pageToken)
@@ -81,17 +99,26 @@ class PerformedQuery:
     async def resolveCommand(self, youtubeClient):
         if self.isThisTypeOfCommand('greet'):
             await self.sendGreetings()
-        elif self.isThisTypeOfCommand('download'):
-            self.setTargetAndParametersForDownloading()
+
+        elif self.isThisTypeOfCommand('update'):
+            numberOfCommentsBefore = db['count']
+            if not self.target and not self.requestParameters:
+                self.setTargetAndParametersForDownloading(sourceOfData.Constants.LAST_MONSIEUR_COMMENT_ID)
             self.downloadAllCommentsFromTargetToDatabase(youtubeClient)
-            await self.sendMessageOnCurrentChannel('Downloaded ' + str(db['count']) + ' comments')
+            await self.sendMessageOnCurrentChannel(
+                'Added ' + str(db['count'] - numberOfCommentsBefore) + ' comments to database')
+
         elif self.isThisTypeOfCommand('show'):
-            commandParameters = getCommandParameters(self.discordMessage.content)
-            await self.showCommentsFromDatabase(commandParameters)
+            commandParameters = getCommandParametersWithSeparator(self.discordMessage.content)
+            if isNumberOfParametersBetween(commandParameters, 1, 2):
+                await self.showCommentsFromDatabase(commandParameters)
+
         elif self.isThisTypeOfCommand('skyrim'):
             await self.sendSkyrimLink()
+
         elif self.isThisTypeOfCommand('help'):
             await self.sendHelpInfo()
+
         else:
             incorrectCommandMsg = "It just doesn't work"
             await self.sendMessageOnCurrentChannel(incorrectCommandMsg)
@@ -108,22 +135,28 @@ class PerformedQuery:
         msg = sourceOfData.skyrimLinkMessage
         await self.sendMessageOnCurrentChannel(msg + link)
 
-    def setTargetAndParametersForDownloading(self):
+    def setTargetAndParametersForDownloading(self, LAST_COMMENT_TO_CHECK):
         TES6_TEASER_ID = 'OkFdqqyI8y4'
         MONSIEUR_DUPOND_CHANNEL_ID = 'UCERhX03EitcqdRRPLOf5tRA'
-        self.setTarget(videoId=TES6_TEASER_ID, channelId=MONSIEUR_DUPOND_CHANNEL_ID)
+        self.target = PerformedQuery.Target(TES6_TEASER_ID, MONSIEUR_DUPOND_CHANNEL_ID, LAST_COMMENT_TO_CHECK)
+
         part = 'snippet'
         textFormat = 'plainText'
         maxResults = 100
         self.setRequestParameters(part, textFormat, maxResults)
 
     def downloadAllCommentsFromTargetToDatabase(self, youtubeClient):
+        allData = []
         while True:
             response = self.getResponseFrom(youtubeClient)
             self.requestParameters.getNewPageTokenFromResponse(response)
-            self.extractCommentsToDatabaseFrom(response)
+            data: List = self.extractCommentsFromResponseToList(response)
+            for comment in data:
+                allData.insert(0, comment)
             if not self.requestParameters.pageToken:
-                return
+                break
+        addToDatabaseFrom(allData)
+        self.setNewestCommentAsOneToStopOn()
 
     def getResponseFrom(self, youtubeClient):
         return youtubeClient.commentThreads().list(videoId=self.target.videoId, part=self.requestParameters.part,
@@ -131,18 +164,78 @@ class PerformedQuery:
                                                    maxResults=self.requestParameters.maxResults,
                                                    pageToken=self.requestParameters.pageToken).execute()
 
-    def extractCommentsToDatabaseFrom(self, responseFromYoutube):
+    def extractCommentsFromResponseToList(self, responseFromYoutube) -> List:
+        data = []
         for commentThread in responseFromYoutube['items']:
             topComment = commentThread['snippet']['topLevelComment']
+            if isCommentFromDeletedChannel(topComment):
+                continue
             if topComment['snippet']['authorChannelId']['value'] == self.target.channelId:
-                db[db['count']] = topComment
-                db['count'] += 1
-                if topComment['id'] == 'UgwSrnCSFi1guLmpS354AaABAg':
+                data.append(topComment)
+                if topComment['id'] == self.target.commentToStopOnId:
                     self.requestParameters.resetPageToken()
-
+                    break
+        return data
 
     async def showCommentsFromDatabase(self, params: List):
-        numberOfSteps = int(params[0])
-        for i in range(numberOfSteps):
-            comment = db[i]
-            await self.sendMessageOnCurrentChannel(convertCommentTextToStr(comment))
+        offset = 0
+        if len(params) == 2:
+            offset = int(params[1])
+            if offset < 0:
+                offset = abs(offset)
+        maxIndex = int(params[0])
+
+        if maxIndex > 0:
+            maxIndex += offset
+            if maxIndex > db['count']:
+                maxIndex = db['count']
+
+            beginning_inclusive = offset
+            end_exclusive = maxIndex
+            step = 1
+        else:
+            maxIndex = abs(maxIndex) + offset
+            if maxIndex > db['count']:
+                maxIndex = db['count']
+
+            beginning_inclusive = db['count'] - 1 - offset
+            end_exclusive = db['count'] - 1 - maxIndex
+            step = -1
+
+        if -1 < beginning_inclusive < db['count'] and -1 <= end_exclusive <= db['count']:
+            await self.fun(beginning_inclusive, end_exclusive, step)
+            await self.sendMessageOnCurrentChannel('Here you go')
+            return
+        await self.sendMessageOnCurrentChannel('There are no comments like these')
+
+    async def fun(self, beginning_inclusive: int, end_exclusive: int, step: int):
+        msg = ''
+        for i in range(beginning_inclusive, end_exclusive, step):
+            comment = db[str(i)]
+            msg += convertCommentTextToStr(comment)
+            if i == end_exclusive - 1 or i == end_exclusive + 1:
+                break
+            if not i % sourceOfData.Constants.NUMBER_OF_COMMENTS_IN_ONE_MSG:
+                await self.sendMessageOnCurrentChannel(msg)
+                msg = ''
+        await self.sendMessageOnCurrentChannel(msg)
+
+    def setNewestCommentAsOneToStopOn(self):
+        indexOfLastComment = str(db['count'] - 1)
+        self.target.commentToStopOnId = db[indexOfLastComment]['snippet']['authorChannelId']['value']
+
+
+def revertIndicesInList(data: List) -> List:
+    indicesToRevert = len(data) // 2
+    for i in range(indicesToRevert):
+        otherIndex = indicesToRevert - 1 - i
+        tmp = data[i]
+        data[i] = data[otherIndex]
+        data[otherIndex] = tmp
+    return data
+
+
+def addToDatabaseFrom(data: List):
+    for comment in data:
+        db[dbCountAsString()] = comment
+        db['count'] += 1
--- sourceOfData.py
+++ sourceOfData.py
@@ -1,13 +1,16 @@
 helpMessage = f"""```fix
 Available commands:
-!greet - greets
-!download - downloads all comments to database so you can access them faster later on
-!show n - shows n latest comments
-!skyrim - allows you to give Todd more money
-!help - sends an e-mail with crying for help to Elon Musk```"""
+*!greet* - greets
+*!update* - downloads all comments to database so you can access them faster later on
+*!show* *n* *d* - shows *n* latest comments, negative *n* shows newest comments. *d* controls offset of comments shown   
+*!skyrim* - allows you to give Todd more money
+*!help* - sends an e-mail with crying for help to Elon Musk```"""
 
 skyrimLink = 'https://steamcommunity.com/app/72850/discussions/0/1692669912384122796/'
 skyrimLinkMessage = 'Here you go pretty sir'
 
 class Constants:
     COMMAND_PROMPT = '!'
+    NUMBER_OF_COMMENTS_IN_ONE_MSG = 10
+    LAST_MONSIEUR_COMMENT_ID = 'UgwSrnCSFi1guLmpS354AaABAg'
+    DAY_850_LOGBOOK_ID = 'UgxiLioO9beoRnddc4J4AaABAg'
