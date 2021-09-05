1e759367f71b2995346b7757c9abf7ac2e2b7825
BuildTools <unconfigured@null.spigotmc.org>
2021-09-05 11:00:24 +0200

    Removed * from !help command because they didn't make text bold with ```fix ```

--- sourceOfData.py
+++ sourceOfData.py
@@ -1,10 +1,10 @@
 helpMessage = f"""```fix
 Available commands:
-*!greet* - greets
-*!update* - downloads all comments to database so you can access them faster later on
-*!show* *n* *d* - shows *n* latest comments, negative *n* shows newest comments. *d* controls offset of comments shown   
-*!skyrim* - allows you to give Todd more money
-*!help* - sends an e-mail with crying for help to Elon Musk```"""
+!greet - greets
+!update - downloads all comments to database so you can access them faster later on
+!show n d - shows n latest comments, negative n shows newest comments. d controls offset of comments shown   
+!skyrim - allows you to give Todd more money
+!help - sends an e-mail with crying for help to Elon Musk```"""
 
 skyrimLink = 'https://steamcommunity.com/app/72850/discussions/0/1692669912384122796/'
 skyrimLinkMessage = 'Here you go pretty sir'
